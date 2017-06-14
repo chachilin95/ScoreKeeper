87ea35d1cc476402404e432ca3ea2d7e41694fac
Alejandro Figueroa <chachilin95@knights.ucf.edu>
2017-06-14 14:57:22 -0400

    finished lecture 14

--- client/main.js
+++ client/main.js
@@ -2,6 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Meteor} from 'meteor/meteor';
 
+const players = [{
+  _id: '1',
+  name: 'Alex',
+  score: 5
+}, {
+  _id: '2',
+  name: 'McKenna',
+  score: 4
+}, {
+  _id: '3',
+  name: 'Kailynn',
+  score: 7
+}];
+
+const renderPlayers = function () {
+  return players.map(function (player) {
+    return <p key={player._id}>{player.name} has {player.score} point(s).</p>
+  });
+}
+
 Meteor.startup(function () {
 
   let title = "Score Keeper App";
@@ -12,6 +32,7 @@ Meteor.startup(function () {
       <h1>{title}</h1>
       <p>Hello, {name}</p>
       <p>You are great!</p>
+      {renderPlayers()}
     </div>
   );
 
