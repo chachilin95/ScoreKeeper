58802c1d919f9351d16bbc138df4ac0e74aacb11
Alejandro Figueroa <chachilin95@knights.ucf.edu>
2017-06-21 13:17:52 -0400

    finsihed lecture 40

--- imports/ui/TitleBar.js
+++ imports/ui/TitleBar.js
@@ -17,9 +17,11 @@ export default class TitleBar extends React.Component {
 
   render () {
     return (
-      <div>
-        {this.renderTitle()}
-        {this.renderSubtitle()}
+      <div className="title-bar">
+        <div className="wrapper">
+          {this.renderTitle()}
+          {this.renderSubtitle()}
+        </div>
       </div>
     );
   }
