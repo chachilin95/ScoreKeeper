b99be3fcbbb9696b411c923c110f009edd3a0d97
Alejandro Figueroa <chachilin95@knights.ucf.edu>
2017-06-21 11:37:08 -0400

    finished lecture 32

--- client/main.js
+++ client/main.js
@@ -8,18 +8,10 @@ import {Players} from './../imports/api/players';
 //UI Components
 import TitleBar from './../imports/ui/TitleBar';
 import AddPlayer from  './../imports/ui/AddPlayer';
-
-
+import Player from './../imports/ui/Player';
 
 const renderPlayers = (players) => players.map((player) => {
-  return (
-    <p key={player._id}>
-      {player.name} has {player.score} point(s).
-      <button onClick={() => { Players.update( {_id: player._id}, {$inc: {score:1}})}}>+1</button> 
-      <button onClick={() => { Players.update({_id: player._id}, {$inc: {score: -1}})}}>-1</button> 
-      <button onClick={() => { Players.remove({_id: player._id})} }>X</button> 
-    </p>
-  );
+  return <Player key={player._id} player={player}/>; 
 });
 
 Meteor.startup(() => {
---
+++ imports/ui/Player.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import {Players} from './../api/players';
+
+export default class Player extends React.Component {
+
+  incrementScore() { 
+    Players.update(this.props.player._id, {$inc: {score: 1}});
+  }
+
+  decrementScore() {
+    Players.update(this.props.player._id, {$inc: {score: -1}});
+  }
+
+  removePlayer() { 
+    Players.remove(this.props.player._id);
+  }  
+
+  render() {
+    return (
+      <div>
+        <p key={this.key}>
+          {this.props.player.name} has {this.props.player.score} point(s).
+          <button onClick={this.incrementScore.bind(this)}>+1</button> 
+          <button onClick={this.decrementScore.bind(this)}>-1</button> 
+          <button onClick={this.removePlayer.bind(this)}>X</button> 
+        </p>
+      </div>
+    );
+  }
+}
+
+Player.propTypes = {
+  player: PropTypes.object.isRequired
+}
