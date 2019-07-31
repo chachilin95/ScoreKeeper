1f14cb5aa244879b24737156294606dbd8791dbc
Alejandro Figueroa <chachilin95@knights.ucf.edu>
2019-07-31 09:58:40 -0500

    Added animations to player list

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -20,20 +20,20 @@ export default ({ player, rank }: PlayerProps) => {
 
     let itemClassName = `item item--position-${rank}`;
 
-    const createPlayerStats = () => {
+    const playerStatsToString = () => {
         const playerRank = numeral(rank).format('0o');
         const playerScore = player.score;
         const playerScoreLabel = player.score === 1 ? 'point' : 'points';
 
         return `${playerRank} place – ${playerScore} ${playerScoreLabel}`;
-    }
+    };
 
     return (
         <div className={itemClassName}>
             <div className='player'>
                 <div className='player__description'>
                     <h3 className='player__name'>{player.name}</h3>
-                    <p className='player__stats'>{createPlayerStats()}</p>
+                    <p className='player__stats'>{playerStatsToString()}</p>
                 </div>
                 <div className='player__actions'>
                     <button className='button button--round' onClick={incrementScore}>+1</button>
--- src/components/PlayerList.tsx
+++ src/components/PlayerList.tsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
 
 import PlayerItem from './Player';
 
@@ -13,18 +14,84 @@ export default () => {
     const sortedPlayers = getSortedPlayers(players);
 
     const renderPlayerList = () => {
-        if (players.length === 0) {
-            return (
+
+        const animations = {
+
+            pop: {
+                initial: {
+                    scale: 0
+                },
+
+                enter: {
+                    scale: 1
+                },
+
+                transition: {
+                    type: "spring",
+                    damping: 20,
+                    stiffness: 800
+                }
+            },
+
+            slide: {
+                initial: {
+                    x: -300,
+                    opacity: 0,
+                    scale: 0
+                },
+
+                enter: {
+                    x: 0,
+                    opacity: 1,
+                    scale: 1
+                },
+
+                exit: {
+                    x: 300,
+                    opacity: 0
+                },
+
+                transition: {
+                    type: "spring",
+                    damping: 15,
+                    stiffness: 300
+                }
+            }
+        }
+
+        const emptyPlayerListMessage = (
+            <motion.div
+                initial={animations.pop.initial}
+                animate={animations.slide.enter}
+                transition={animations.pop.transition}
+            >
                 <div className='item'>
                     <p className='item__message'>Add a player to the list!</p>
                 </div>
-            );
+            </motion.div>
+        );
+
+        const populatedPlayerList = (
+            <AnimatePresence>
+                {sortedPlayers.map((player: Player, index: number) => (
+                    <motion.div
+                        key={player.id}
+                        initial={animations.slide.initial}
+                        animate={animations.slide.enter}
+                        exit={animations.slide.exit}
+                        positionTransition={animations.slide.transition}
+                    >
+                        <PlayerItem rank={index + 1} player={player} />
+                    </motion.div>
+                ))}
+            </AnimatePresence>
+        );
+
+        // render
+        if (players.length === 0) {
+            return emptyPlayerListMessage;
         } else {
-            return (
-                sortedPlayers.map((player: Player, index: number) => (
-                    <PlayerItem key={player.id} rank={index+1} player={player} />
-                ))                
-            );
+            return populatedPlayerList;
         }
     };
 
